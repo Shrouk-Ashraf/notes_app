cdac287cf45fb11596dd03bbbb61ea1816c450a7
Shrouk <shrouk.a.elsaeid31@gmail.com>
2023-12-07 21:59:53 +0200

    change the place of add note bloc provider to the add note bottom sheet to avoid using many resources

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -9,26 +9,29 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
-      child: BlocConsumer<AddNoteCubit, AddNoteState>(
-        listener: (context, state) {
-          if(state is AddNoteFailure){
-            print('failed');
-          }
-          if(state is AddNoteSuccess){
-            print('success');
-            Navigator.pop(context);
-          }
-        },
-        builder: (context, state) {
-          return ModalProgressHUD(
-            inAsyncCall: state is AddNoteLoading ? true : false,
-              child:  const SingleChildScrollView(
+    return BlocProvider(
+      create: (context) => AddNoteCubit(),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteFailure) {
+              print('failed');
+            }
+            if (state is AddNoteSuccess) {
+              print('success');
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+                inAsyncCall: state is AddNoteLoading ? true : false,
+                child: const SingleChildScrollView(
                   child: AddNoteForm(),
-              )
-          );
-        },
+                )
+            );
+          },
+        ),
       ),
     );
   }
