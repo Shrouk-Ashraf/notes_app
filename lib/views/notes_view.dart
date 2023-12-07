a1c07f447475dea9e7ef13de618f3f3ba0e033b7
Shrouk <shrouk.a.elsaeid31@gmail.com>
2023-12-08 00:03:52 +0200

    add scroll control to show modal bottom sheet to make the bottom sheet above the keyboard

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -15,6 +15,7 @@ class NotesView extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
           onPressed: (){
             showModalBottomSheet(
+              isScrollControlled: true,
                 context: context,
                 builder: (context){
                   return Padding(
