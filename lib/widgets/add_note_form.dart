4c74512340cf977c05a81d2ac0c6d2935619cfe0
Shrouk <shrouk.a.elsaeid31@gmail.com>
2023-12-08 01:26:19 +0200

    format date

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 
@@ -53,10 +54,11 @@ class _AddNoteFormState extends State<AddNoteForm> {
                   if (formKey.currentState!.validate()) {
                     formKey.currentState!
                         .save(); //to call onSaved method in textFormField
+                    String date = DateFormat.yMMMd().format(DateTime.now());
                     var noteModel = NoteModel(
                         title: title!,
                         subTitle: subTitle!,
-                        date: DateTime.now().toString(),
+                        date: date,
                         color: Colors.red.value);
                     BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
                   } else {
