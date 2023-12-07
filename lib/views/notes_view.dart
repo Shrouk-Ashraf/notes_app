6db950b7d3d663242aced000478a1596611875d6
Shrouk <shrouk.a.elsaeid31@gmail.com>
2023-12-08 00:23:30 +0200

    provide show notes cubit

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,6 +1,7 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/cubits/show_notes_cubit/show_notes_cubit.dart';
 import 'package:notes_app/widgets/add_note_bottom_sheet.dart';
 
 import '../widgets/notes_view_body.dart';
@@ -9,29 +10,36 @@ class NotesView extends StatelessWidget {
   const NotesView({Key? key}) : super(key: key);
 
   static const String id = 'NotesView';
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      floatingActionButton: FloatingActionButton(
-          onPressed: (){
+    return BlocProvider(
+      create: (context) => ShowNotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
             showModalBottomSheet(
-              isScrollControlled: true,
+                isScrollControlled: true,
                 context: context,
-                builder: (context){
+                builder: (context) {
                   return Padding(
-                    padding:  EdgeInsets.only(
-                        bottom: MediaQuery.of(context).viewInsets.bottom
+                    padding: EdgeInsets.only(
+                        bottom: MediaQuery
+                            .of(context)
+                            .viewInsets
+                            .bottom
                     ),
                     child: const AddNoteBottomSheet(),
                   );
                 }
             );
           },
-        shape: const CircleBorder(),
-        backgroundColor: kPrimaryColor,
-        child: const Icon(Icons.add,color: Colors.black,),
-      ),
+          shape: const CircleBorder(),
+          backgroundColor: kPrimaryColor,
+          child: const Icon(Icons.add, color: Colors.black,),
+        ),
         body: const NotesViewBody(),
+      ),
     );
   }
 }
