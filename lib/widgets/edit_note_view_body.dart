fd616f22baa3e5f1d240e79cea922351fdc720c3
Shrouk <shrouk.a.elsaeid31@gmail.com>
2023-12-08 02:29:20 +0200

    remove final for values that will be changed in note model & convert edit view body to stateful

--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -3,21 +3,43 @@ import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
 import 'custom_app_bar.dart';
-class EditNoteViewBody extends StatelessWidget {
+class EditNoteViewBody extends StatefulWidget {
   const EditNoteViewBody({Key? key, required this.note}) : super(key: key);
 
   final NoteModel note;
+
+  @override
+  State<EditNoteViewBody> createState() => _EditNoteViewBodyState();
+}
+
+class _EditNoteViewBodyState extends State<EditNoteViewBody> {
+  String? title, subTitle;
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 24),
+    return  Padding(
+      padding:const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
-          CustomAppBar(title: 'Edit Note',icon: Icons.check,),
-          SizedBox(height: 16,),
-          CustomTextFormField(hintText: "title"),
-          SizedBox(height: 16,),
-          CustomTextFormField(hintText: 'Content',maxLines: 5,),
+          CustomAppBar(title: 'Edit Note',icon: Icons.check,
+            onPressed: (){
+
+            },
+          ),
+          const SizedBox(height: 16,),
+          CustomTextFormField(
+              onChanged: (value){
+                title = value;
+              },
+              hintText: "title"
+          ),
+          const SizedBox(height: 16,),
+          CustomTextFormField(
+            onChanged: (value){
+              subTitle = value;
+            },
+            hintText: 'Content',
+            maxLines: 5,
+          ),
 
         ],
       ),
